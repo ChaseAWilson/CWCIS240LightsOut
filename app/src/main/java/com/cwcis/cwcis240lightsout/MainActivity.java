b11f63f4510fa6df3f84f533e0bba4ff422f46ee
CHASE WILSON <cw0939659@otc.edu>
2023-04-10 01:20:07 -0500

    Initial commit

---
+++ app/src/main/java/com/cwcis/cwcis240lightsout/MainActivity.java
@@ -0,0 +1,87 @@
+package com.cwcis.cwcis240lightsout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LightsOutGame mGame;
+    private GridLayout mLightGrid;
+    private int mLightOnColor;
+    private int mLightOffColor;
+    private int mLightOffText;
+    private int mLightOnText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLightGrid = findViewById(R.id.light_grid);
+
+        // Add the same click handler to all grid buttons
+        for (int buttonIndex = 0; buttonIndex < mLightGrid.getChildCount(); buttonIndex++) {
+            Button gridButton = (Button) mLightGrid.getChildAt(buttonIndex);
+            gridButton.setOnClickListener(this::onLightButtonClick);
+        }
+
+        mLightOnColor = ContextCompat.getColor(this, R.color.yellow);
+        mLightOffColor = ContextCompat.getColor(this, R.color.black);
+        mLightOffText = R.string.lightOffText;
+        mLightOnText = R.string.lightOnText;
+
+        mGame = new LightsOutGame();
+        startGame();
+    }
+
+    private void startGame() {
+        mGame.newGame();
+        setButtonColors();
+    }
+
+    private void onLightButtonClick(View view) {
+
+        // Find the button's row and col
+        int buttonIndex = mLightGrid.indexOfChild(view);
+        int row = buttonIndex / LightsOutGame.GRID_SIZE;
+        int col = buttonIndex % LightsOutGame.GRID_SIZE;
+
+        mGame.selectLight(row, col);
+        setButtonColors();
+
+        // Congratulate the user if the game is over
+        if (mGame.isGameOver()) {
+            Toast.makeText(this, R.string.congrats, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setButtonColors() {
+
+        for (int buttonIndex = 0; buttonIndex < mLightGrid.getChildCount(); buttonIndex++) {
+            Button gridButton = (Button) mLightGrid.getChildAt(buttonIndex);
+
+            // Find the button's row and col
+            int row = buttonIndex / LightsOutGame.GRID_SIZE;
+            int col = buttonIndex % LightsOutGame.GRID_SIZE;
+
+            if (mGame.isLightOn(row, col)) {
+                gridButton.setBackgroundColor(mLightOnColor);
+                gridButton.setText(mLightOnText);
+                gridButton.setTextColor(mLightOffColor);
+            } else {
+                gridButton.setBackgroundColor(mLightOffColor);
+                gridButton.setText(mLightOffText);
+                gridButton.setTextColor(mLightOnColor);
+            }
+        }
+    }
+
+    public void onNewGameClick(View view) {
+        startGame();
+    }
+}
